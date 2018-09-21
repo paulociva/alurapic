955e8313d35779de4277e40781d90427424a4026
Paulo Eduardo Civa <paulociva@hotmail.com>
2018-09-21 13:23:26 -0300

    Removido pasta invalida e corrigidos arquivos.

--- src/app/shared/directives/immediate-click/immediate-click.module.ts
+++ src/app/shared/directives/immediate-click/immediate-click.module.ts
@@ -1,6 +1,6 @@
-import { NgModule } from "@angular/core";
-import { ImmediateClickDirective } from "./immediate-click.directive";
-import { CommonModule } from "@angular/common";
+import { NgModule } from '@angular/core';
+import { ImmediateClickDirective } from './immediate-click.directive';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
     declarations: [ImmediateClickDirective],
